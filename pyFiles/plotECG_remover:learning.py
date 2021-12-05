1e46caf86fba5fdb231a9f668292b0d4c30f9a5d
Ramon Suwanban <ramon_suwanban_lily@hotmail.com>
2021-12-05 21:27:12 +0000

    edit typo

--- pyFiles/plotECG_remover:learning.py
+++ pyFiles/plotECG_remover:learning.py
@@ -34,7 +34,7 @@ pyplot.xlabel('Sample Number')
 pyplot.ylabel('Amplitude') 
 
 pyplot.figure()
-pyplot.plot(remover[delayLineLength+preFilterBuffer:len(data2)])
+pyplot.plot(remover[delayLineLength+preFilterBuffer:len(remover)])
 pyplot.title('Remover')
 pyplot.xlabel('Sample Number')
 pyplot.ylabel('Amplitude') 
